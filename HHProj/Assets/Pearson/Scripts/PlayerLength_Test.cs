c79750bcec2c22d30329e2a17e3b9fdb80718ccf
PearsonLawrence <pearson.lawrence.official@gmail.com>
2023-11-24 19:50:21 -0800

    Started simple networking tutorial

---
+++ HHProj/Assets/Pearson/Scripts/PlayerLength_Test.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerLength_Test : NetworkBehaviour
+{
+    public NetworkVariable<ushort> length = new(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+
+    private List<GameObject> _tails;
+    private Transform _lastTail;
+    [SerializeField]
+    private GameObject tailPrefab;
+
+    private Collider thisCollider;
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        _tails = new List<GameObject>();
+        _lastTail = transform;
+        thisCollider = GetComponent<Collider>();
+        if(!IsServer) length.OnValueChanged += LengthChanged;
+    }
+
+    [ContextMenu("Add Length")]
+    //Only called by server
+    private void AddLength()
+    {
+        length.Value += 1;
+        InstantiateTail();
+    }
+
+    private void LengthChanged(ushort previousValue, ushort newValue)
+    {
+        Debug.Log("LengthChanged callback");
+        InstantiateTail();
+    }
+
+    private void InstantiateTail()
+    {
+        GameObject tail = Instantiate(tailPrefab, transform.position, Quaternion.identity);
+        
+        if (tail.TryGetComponent(out Tail tailOut))
+        {
+            tailOut.networkedOwner = transform;
+            tailOut.followTransform = _lastTail;
+            _lastTail = tail.transform;
+            Physics.IgnoreCollision(tail.GetComponent<Collider>(), thisCollider);
+        }
+        _tails.Add(tail);
+    }
+}
