bb66c30e611a00494ccdf247cc274ccedc0f2654
m-smith234 <msmith234@nevada.unr.edu>
2024-03-05 19:55:51 -0800

    Added Cast/Wall/Teleport Gestures
    Added Left and Right Hand compatibility for Cast and Wall, Teleport is Two-Handed.

--- Assets/Mason/Scripts/GestureEventProcessor.cs
+++ Assets/Mason/Scripts/GestureEventProcessor.cs
@@ -22,16 +22,36 @@ public class GestureEventProcessor : MonoBehaviour
         //Negative values are errors, so this throws an error if the gesture was not read properly
         if (gestureCompletionData.gestureID < 0) {
             string errorMessage = GestureRecognition.getErrorMessage(gestureCompletionData.gestureID);
-            // ...
             return;
         }
         //Specifies how similar gestures made in game must be to pre-recorded gesture samples
-        if (gestureCompletionData.similarity >= 0.3) {
-            //Casts Wall Spell
-            if (gestureCompletionData.gestureName == "Wall") {
-                Debug.Log("Wall Gesture Successfully Casted");
+        if (gestureCompletionData.similarity >= 0.5) {
+            //Casts Left Hand Wall Spell
+            if (gestureCompletionData.gestureName == "Left Wall") {
+                Debug.Log("Left Wall Gesture Successfully Casted");
+            }
+            //Casts Right Hand Wall Spell
+            if (gestureCompletionData.gestureName == "Right Wall")
+            {
+                Debug.Log("Right Wall Gesture Successfully Casted");
+            }
+            //Casts Left Hand Cast Spell
+            if (gestureCompletionData.gestureName == "Left Cast")
+            {
+                Debug.Log("Left Cast Gesture Successfully Casted");
+            }
+            //Casts Right Hand Cast Spell
+            if (gestureCompletionData.gestureName == "Right Cast")
+            {
+                Debug.Log("Right Cast Gesture Successfully Casted");
+            }
+            //Casts Teleport
+            if (gestureCompletionData.gestureName == "Teleport")
+            {
+                Debug.Log("Teleport Gesture Successfully Casted");
             }
         }
+
         else {
             Debug.Log("Gesture Failed to Cast");
         }
