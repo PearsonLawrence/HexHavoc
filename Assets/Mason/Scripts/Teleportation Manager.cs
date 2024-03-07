e6d653ecb6bad7985e5e1faae3f87ae26501c1d5
m-smith234 <msmith234@nevada.unr.edu>
2024-03-06 20:10:00 -0800

    Teleportation Test
    Went from hand-based teleportation to camera based, pushing to merge to test if teleportation works.

--- Assets/Mason/Scripts/Teleportation Manager.cs
+++ Assets/Mason/Scripts/Teleportation Manager.cs
@@ -8,16 +8,19 @@ public class TeleportationManager : MonoBehaviour
     public GameObject teleportationRayPrefab;
 
     private GameObject teleportationRay;
-    private Transform leftHand;
-    private Transform rightHand;
+    private Transform playerHead;
     private GestureEventProcessor gestureEventProcessor;
 
     // Start is called before the first frame update
     void Start()
     {
-        leftHand = GameObject.Find("Left Controller").transform;
-        rightHand = GameObject.Find("Right Controller").transform;
         gestureEventProcessor = GetComponent<GestureEventProcessor>();
+        playerHead = Camera.main.transform;
+
+        if (playerHead == null)
+        {
+            Debug.Log("Player Camera Reference not set in TeleportationManager");
+        }
     }
 
     void Update()
@@ -35,14 +38,14 @@ public class TeleportationManager : MonoBehaviour
         RaycastHit hit;
         Vector3 teleportDestination;
 
-        //Uses direction vector from left hand to right hand as the ray direction
-        Vector3 rayDirection = (rightHand.up + leftHand.up).normalized;
-
-        //Calculates ray's starting position at center of the hands during the gesture
-        Vector3 rayStartPosition = (rightHand.position + leftHand.position) / 2f;
+        //Uses direction vector from where player is currently facing
+        Vector3 rayDirection = playerHead.forward;
 
         //Creates ray from left hand position
-        Ray ray = new Ray(leftHand.position, rayDirection);
+        Ray ray = new Ray(playerHead.position, rayDirection);
+
+        //Calculates ray's starting position at player's head during the gesture
+        Vector3 rayStartPosition = playerHead.position;
 
         Debug.DrawRay(rayStartPosition, rayDirection*1000, Color.red, 30);
 
