8b15a91bad3e64537a1fa384ff87a85bb558d017
PearsonLawrence <pearson.lawrence.official@gmail.com>
2024-04-19 23:50:56 -0700

    Art Art Art and Gesture framework

---
+++ Assets/Pearson/PearsonScripts/Player Scripts/RigSyncComponent.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigSyncComponent : MonoBehaviour
+{
+    public GameObject RigLHand;
+    public GameObject RigRHand;
+    public GameObject RigHead;
+    public GameObject NetLHand;
+    public GameObject NetRHand;
+    public GameObject NetHead;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        RigLHand.gameObject.transform.position = NetLHand.transform.position;
+        RigRHand.gameObject.transform.position = NetRHand.transform.position;
+        RigHead.gameObject.transform.position = NetHead.transform.position;
+        RigLHand.gameObject.transform.rotation = NetLHand.transform.rotation;
+        RigRHand.gameObject.transform.rotation = NetRHand.transform.rotation;
+        RigHead.gameObject.transform.rotation = NetHead.transform.rotation;
+    }
+}
